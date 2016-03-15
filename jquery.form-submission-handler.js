24c6d19e973cd3786608bc20422a23cac62c5018
Jake Howell <jhowell@skeduna.com>
2016-03-15 13:41:23 -0600

    Adding jquery version of form submission handler

---
+++ jquery.form-submission-handler.js
@@ -0,0 +1,32 @@
+$(function(){
+  var forms = $('body').find('.gform');
+  $(forms).on('submit', handleFormSubmit);
+});
+
+function getFormData(form) {
+  var elements = $(form).find('input'); // all form elements
+  var data = {};
+  $(elements).each(function(index,element){
+    data[element.name] = element.value;
+  });
+  return data;
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var form = $(event.target);
+  var data = getFormData(form);
+  var encoded = Object.keys(data).map(function(k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+  }).join('&');
+
+  $.ajax({
+    type: 'POST',
+    url: event.target.action,
+    data: encoded,
+    success: function(data, status, xhr){
+      form.hide();
+      form.parent().find('.thank-you-message').show();
+    }
+  });
+}
